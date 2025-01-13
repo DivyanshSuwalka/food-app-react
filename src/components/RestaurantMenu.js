f7706694c915ea7039930b83bfbf9182b26a0dea
DivyanshSuwalka <suwalkadivyansh63@gmail.com>
2025-01-14 00:03:20 +0530

    useRestaurant

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -6,6 +6,7 @@ import { IMG_CDN_URL } from "../constants";
 import { addItem } from "../utils/cartSlice";
 import { useDispatch } from "react-redux";
 import { log } from "console";
+import { FETCH_MENU_URL } from "../constants";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
@@ -17,7 +18,27 @@ const RestaurantMenu = () => {
     console.log(info);
   };
 
-  const menu = useRestaurant(resId);
+  // const menu = useRestaurant(resId);
+  [menu, setMenu] = useState(null);
+
+  useEffect(() => {
+    getRestaurantInfo();
+  }, []);
+
+  async function getRestaurantInfo() {
+    const data = await fetch(
+      FETCH_MENU_URL +
+        resId + //resId:78944
+        "&catalog_qa=undefined&submitAction=ENTER"
+    );
+    const json = await data.json();
+    console.log(json.data);
+    // setRestaurant(json.data?.cards[2]?.card?.card?.info);
+    setMenu(
+      json?.data?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
+        ?.card?.itemCards
+    );
+  }
 
   console.log(menu);
 
